51124b2288aa8b2e3979783eeca9f2d37814d513
Daniel Bludeau <dbludeau@skld.com>
2016-12-14 09:58:08 -0700

    Add project files.

---
+++ winAliasLauncher/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Diagnostics;
+using WpfApplication1.models;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // load up the json file of options
+        fileParser fp = new fileParser();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            string msg = entryBox.Text;
+            string cmd = fp.getCmd(msg);
+
+            try
+            {
+                // need to add full process start up (so we can add admin start)
+                ProcessStartInfo p = new ProcessStartInfo(cmd);
+                p.Verb = "runas";
+                p.UseShellExecute = true;
+                Process.Start(p);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Unable to start matching program for: " + msg + " (" + err.Message + ")");
+            }
+        }
+    }
+}
---
+++ winAliasLauncher/models/fileParser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Windows;
+
+namespace WpfApplication1.models
+{
+    class fileParser
+    {
+        static string path = Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%");
+        static string json_file = path + "\\launcherPrograms.json";
+        //static string json_file = "C:\\Users\\dbludeau\\launcherPrograms.json";
+        private JObject jo;
+
+        public fileParser()
+        {
+            if (File.Exists(json_file))
+            {
+                this.jo = JObject.Parse(File.ReadAllText(json_file));
+            }
+            else
+            {
+                MessageBox.Show("Unable to load json data file (" + json_file + ")");
+            }
+        }
+
+        public string getCmd(string name)
+        {
+            return (string)this.jo[name];
+        }
+    }
+}
